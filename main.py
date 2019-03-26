e01baea07e5b6f5f165a573f311b16908bd66982
Kiran Gopinathan <zcabkgo@ucl.ac.uk>
2019-03-26 07:40:58 +0000

    implemented fadable sound mixing

--- main.py
+++ main.py
@@ -1,72 +1,76 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import scipy.fftpack
+from pathlib import Path
+
+import pygame
+from pygame.mixer import Sound
 
 import time
-from threading import Thread
+from threading import Thread, Event
 import queue
 
 from pynput.keyboard import Key, Listener
+from music_manager import MusicManager
+from keyboard_handler import KeyboardBeatDetector
 
-start_time = time.time()
-last_reference_time = time.time()
-REFERENCE_WINDOW_TIME = 0.3
-current_count = 0
-inputs_log = []
-
+pygame.mixer.init()
+pygame.init()
 
 
 
-def on_press(key):
+def play_song(filename, fadein=1000, fadeout=1000, callback=None):
+    stop_event = Event()
+    def play():
+        sound_a = Sound(filename)
+        sound_length = sound_a.get_length()
+
+        sound_a.play(fade_ms=fadein)
+        # wait for a stop message or for song to end
+        stop_event.wait(sound_length)
+
+        sound_a.fadeout(fadeout)
+
+        if callback:
+            callback(filename)
+
+    t = Thread(target=play)
+    t.daemon = True
+    t.start()
+
+    return stop_event
+
+
+
+
+class MusicPlayer:
     """
-    Called whenever a key is pressed.
+    Plots beats from a queue
     """
-    global last_reference_time
-    global current_count
-    global inputs_log
 
-    # capture special keys
-    if isinstance(key, Key):
-        print('Special key {}'.format(key))
-        Key.alt
-    else:
-        # get the current time
-        current_time = time.time()
+    def __init__(self, beat_queue):
+        self.beat_queue = beat_queue
 
-        print('Time between keyboard inputs: ', current_time - last_reference_time)
+        self.internal_counts = []
+        self.internal_times = []
 
-        # de-bounce inputs
-        if current_time - last_reference_time > REFERENCE_WINDOW_TIME:
-            inputs_log.append((last_reference_time, current_count))
+    def run(self):
 
-            current_count = 1
-            last_reference_time = current_time
-        else:
-            current_count += 1
+        while True:
 
+            # retrieve the next entry
+            (count, time) = self.beat_queue.get()
 
-def on_release(key):
-    print('{} release'.format(key))
+            self.internal_times.append(time)
+            self.internal_counts.append(count)
 
-    if key == Key.esc:
-        return False
+stop_song_a = play_song(song_a)
+time.sleep(10)
+print('starting next song')
+stop_song_b = play_song(song_b)
+time.sleep(10)
+print('stopping first song')
+stop_song_a.set()
 
-if __name__ == '__main__':
-
-    print('Hello world!')
-    with Listener(
-            on_press = on_press,
-            on_release = on_release
-    ) as listener:
-        listener.join()
-
-
-    inputs_log = np.array(inputs_log)
-    plt.plot(inputs_log[:,0], inputs_log[:,1])
-    plt.show()
-    yf = scipy.fftpack.fft(inputs_log[:,1])
-    xf = scipy.fftpack.fftfreq(yf.shape[0], REFERENCE_WINDOW_TIME)
-    plt.plot(xf, yf)
-    plt.show()
-
-
+time.sleep(2)
+time.sleep(100)
